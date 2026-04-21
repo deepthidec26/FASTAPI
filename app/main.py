2e7f00f44c0908c12b57e3b9f131146321b30dbf
deepthidec26 <deepthiv2829@gmail.com>
2026-04-21 16:47:17 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
+ifrom fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello everyone. This is Sai Shashank Yerra! How are you doing? I'm good. Thank you for asking."}
+    return {"message": "Hello everyone. This is Deepthi! How are you doing? I'm good. Thank you for asking."}
 
 @app.get("/health")
 def health():
